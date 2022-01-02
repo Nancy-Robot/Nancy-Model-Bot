d6aef23e37eb55314f9e7d7d05138ad12b3299cc
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-01-02 15:22:58 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,7 +1,14 @@
 import logging
+from typing import Union
+from typing import Optional, List
+import pyrogram
+from struct import pack
+from pyrogram import raw
+from pyrogram.file_id import FileId, FileType, PHOTO_TYPES, DOCUMENT_TYPES
+from pyrogram import raw, types, utils
 from pyrogram import Client, emoji, filters
 from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import get_search_results
 from utils import is_subscribed, get_size 
 from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
