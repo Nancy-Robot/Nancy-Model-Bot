9b0598eeaf8940ea50a7136c6c2dbd9e04f058ca
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-01-29 23:17:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -922,14 +922,14 @@ async def auto_filter(client, msg, spoll=False):
             [InlineKeyboardButton(text=f"🗓 1/{round(int(total_results)/10)}",callback_data="pages"), InlineKeyboardButton(text="NEXT »",callback_data=f"next_{req}_{key}_{offset}")]
         )
         btn.insert(0,
-            [InlineKeyboardButton(text="⭕️ {search}",url="https://t.me/+CeY_RGCtK1g0ZWQ9")]
+            [InlineKeyboardButton(text="🔱 {title} 🔱",url="https://t.me/+CeY_RGCtK1g0ZWQ9")]
         )
     else:
         btn.append(
             [InlineKeyboardButton(text="🗓 1/1 🗓",callback_data="pages")]
         )
         btn.insert(0,
-            [InlineKeyboardButton(text="⭕️ {search} ⭕️",url="https://t.me/+CeY_RGCtK1g0ZWQ9")]
+            [InlineKeyboardButton(text="⭕️ {title} ⭕️",url="https://t.me/+CeY_RGCtK1g0ZWQ9")]
         )
     reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
     imdb = await get_poster(search, file=(files[0]).file_name) if IMDB else None
