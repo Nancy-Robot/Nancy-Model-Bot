2ee8a3d988a17da32de61a1c0e61db741c507d29
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-01-30 00:11:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -421,7 +421,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             )
 
     elif query.data == "pages":
-        await query.answer("⚠️ Hey, {query.from_user.first_name}! Sorry, This Button Is Not You 😔",show_alert=True)
+        await query.answer(f" ⚠️ Hey, {query.from_user.first_name}! Sorry, This Button Is Not You 😔",show_alert=True)
     elif query.data == "start":
         buttons = [[
             InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
